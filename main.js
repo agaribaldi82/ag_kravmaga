ceef36299ccb85a41007080b83920987706bd573
agaribaldi82 <adgaribaldi@gmail.com>
2024-04-30 18:02:25 -0300

    agruegue archivo js

---
+++ main.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const menu_btn = document.getElementById("menu_hamburguesa");
+    const menu_list = document.getElementById("lista_menu");
+
+    menu_btn.addEventListener("click", function() {
+        toggle(menu_list, "active");
+    });
+
+    function toggle(element, className = "active") {
+        element.classList.toggle(className);
+    }
+});
