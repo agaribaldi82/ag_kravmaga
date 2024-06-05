4d4040725e1843caf506d2a12526b9ee40bc447e
agaribaldi82 <adgaribaldi@gmail.com>
2024-06-05 11:59:03 -0300

    Correcciones varias

--- form.js
+++ form.js
@@ -1,8 +1,8 @@
-let enviar =document.querySelector("#enviar")
+let enviar = document.querySelector("#enviar")
 
 if (enviar) {
     enviar.addEventListener("click", function() {
         alert("¡Gracias por contactarnos! En breve responderemos su consulta.");
-        window.location.href="../index.html"  
+        window.location.href = "index.html";
     });
 }
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+/*document.addEventListener("DOMContentLoaded", function() {
     const menu_btn = document.getElementById("menu_hamburguesa");
     const menu_list = document.getElementById("lista_menu");
 
@@ -9,4 +9,27 @@ document.addEventListener("DOMContentLoaded", function() {
     function toggle(element, className = "active") {
         element.classList.toggle(className);
     }
-});
+});*/
+
+function abrir(){
+    document.getElementById("lista_menu").style.display="block"
+    document.getElementById("menu_hamburguesa").style.display="none"
+}
+
+function cerrar(){
+    document.getElementById("lista_menu").style.display="none";
+    document.getElementById("menu_hamburguesa").style.display="flex"
+}
+
+function login(){
+    document.getElementById("formu").style.display="flex"
+}
+
+function cerrar_form(){
+    document.getElementById("formu").style.display="none"
+}
+
+let volver = document.getElementById("volver")
+volver.addEventListener("click", ()=>{
+    window.location.href = "index.html"
+})
