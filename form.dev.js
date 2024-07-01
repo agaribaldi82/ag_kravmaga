88e4a734566c1b25c422ebca54c80dc13906ce8f
agaribaldi82 <adgaribaldi@gmail.com>
2024-07-01 15:37:24 -0300

    Formulario de contacto y formulario de datos funcionando correctamente, falta formulario de instructor

--- form.dev.js
+++ form.dev.js
@@ -7,3 +7,23 @@ if (enviar) {
     alert("¡Gracias por contactarnos! En breve responderemos su consulta."); //window.location.href = "index.html";
   });
 }
+
+var enviar_datos = document.getElementById("btn");
+enviar_datos.addEventListener("click", function (event) {
+  var nombre = document.getElementById("name").value.trim();
+  var apellido = document.getElementById("lastname").value.trim();
+  var tel = document.getElementById("tel").value.trim();
+  var fecha_nac = document.getElementById("fecha_nac").value.trim();
+  var ci = document.getElementById("ci").value.trim();
+  var sociedad = document.getElementById("sociedad").value.trim();
+  var emergencia = document.getElementById("emergencia").value.trim();
+  var email = document.getElementById("email").value.trim();
+
+  if (nombre === "" || apellido === "" || tel === "" || fecha_nac === "" || ci === "" || sociedad === "" || emergencia === "" || email === "") {
+    event.preventDefault();
+    alert("Por favor, complete todos los campos antes de enviar el formulario.");
+    window.location.href = "https://www.kravmagauy.com";
+  } else {
+    alert("Sus datos fueron ingresados con éxito");
+  }
+});
--- form.js
+++ form.js
@@ -6,3 +6,24 @@ if (enviar) {
         //window.location.href = "index.html";
     });
 }
+
+const enviar_datos = document.getElementById("btn")
+    
+enviar_datos.addEventListener("click", (event)=>{
+    let nombre = document.getElementById("name").value.trim();
+    let apellido = document.getElementById("lastname").value.trim();
+    let tel = document.getElementById("tel").value.trim();
+    let fecha_nac = document.getElementById("fecha_nac").value.trim();
+    let ci = document.getElementById("ci").value.trim();
+    let sociedad = document.getElementById("sociedad").value.trim();
+    let emergencia = document.getElementById("emergencia").value.trim();
+    let email = document.getElementById("email").value.trim();
+
+    if (nombre === ""|| apellido === ""|| tel === ""|| fecha_nac === ""|| ci === ""|| sociedad === ""|| emergencia === ""|| email === ""){
+        event.preventDefault()
+        alert("Por favor, complete todos los campos antes de enviar el formulario.");
+        window.location.href = "https://www.kravmagauy.com"
+
+    } else {alert("Sus datos fueron ingresados con éxito")}
+    
+})
